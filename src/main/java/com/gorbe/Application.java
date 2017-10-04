a15ddb1f0b95d6a7e1c69f86b63d7e0a9750c7fd
SinglePi <32506223+singlepi@users.noreply.github.com>
2017-10-04 22:52:14 +0200

    Clean the project from useless files.

--- src/main/java/com/gorbe/Application.java
+++ src/main/java/com/gorbe/Application.java
@@ -9,4 +9,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    /**
+     * @Controller
+     * resources/templates folder
+     * return as String
+     * **/
 }
--- src/main/java/com/gorbe/controller/NothingController.java
+++ src/main/java/com/gorbe/controller/NothingController.java
@@ -3,23 +3,18 @@ package com.gorbe.controller;
 import com.gorbe.model.nothing_model.Quote;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/api")
 public class NothingController {
 
-
-    @RequestMapping("/text")
-    public String text(@RequestParam(value="text", defaultValue=" ") String text) {
-        return text;
-    }
-
     @RequestMapping(value = "/quote", method = RequestMethod.GET)
-    public String quote(){
+    public Quote quote(){
         RestTemplate restTemplate = new RestTemplate();
         Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        return quote.toString();
+        System.out.println(quote.toString());
+        return quote;
     }
 }
